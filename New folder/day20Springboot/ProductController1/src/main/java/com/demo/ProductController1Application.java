46eb031c2e09268cbfe889b6216fc9fdfae3b196
karthik-ctrl <karthikmudiraj520@gmail.com>
2022-03-14 10:59:16 +0530

    springboot

---
+++ New folder/day20Springboot/ProductController1/src/main/java/com/demo/ProductController1Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductController1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductController1Application.class, args);
+	}
+
+}
---
+++ New folder/day20Springboot/springboot_helloworld/src/main/java/com/demo/SpringbootHelloworldApplication.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHelloworldApplication.class, args);
+		System.out.println("spring boot running");
+		
+	}
+
+}
